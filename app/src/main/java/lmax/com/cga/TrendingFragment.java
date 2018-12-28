5d54cdc0788e3d723734ff6b69021c87709ebf0f
AymaneMx <aymanemx@gmail.com>
2018-12-28 20:59:58 +0000

    add gridView
    the gridView is ready now to get data from Github Api:
    - set the gridView
    - add the gridView adapter
    - add Picasso library

--- app/src/main/java/lmax/com/cga/TrendingFragment.java
+++ app/src/main/java/lmax/com/cga/TrendingFragment.java
@@ -5,17 +5,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ProgressBar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lmax.com.cga.model.Item;
 
 
 public class TrendingFragment extends Fragment {
+
+    private RepoAdapter mAdapter;
+
+    private ProgressBar mProgressBar;
+
+    private List<Item> streamData ;
 
     public TrendingFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate( R.layout.fragment_trending, container, false );
+        View rootView = inflater.inflate( R.layout.fragment_trending, container, false );
+
+        mProgressBar = rootView.findViewById(R.id.progressBar);
+
+        // Create the array adapter and bind it to the GridView
+        GridView gridView = rootView.findViewById(R.id.grid);
+
+        streamData = new ArrayList<>();
+
+        mAdapter = new RepoAdapter(getContext(), 0, streamData);
+        gridView.setAdapter(mAdapter);
+
+        return  rootView;
     }
 
 
