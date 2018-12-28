aa345e9a1c0d59c0e1006767d2b1e082a95a2011
AymaneMx <aymanemx@gmail.com>
2018-12-28 18:13:02 +0000

    add models and Gson library

---
+++ app/src/main/java/lmax/com/cga/model/ListRepoData.java
@@ -0,0 +1,44 @@
+package lmax.com.cga.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ListRepoData {
+
+    @SerializedName("total_count")
+    @Expose
+    private int totalCount;
+    @SerializedName("incomplete_results")
+    @Expose
+    private boolean incompleteResults;
+    @SerializedName("items")
+    @Expose
+    private List<Item> items = null;
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public boolean isIncompleteResults() {
+        return incompleteResults;
+    }
+
+    public void setIncompleteResults(boolean incompleteResults) {
+        this.incompleteResults = incompleteResults;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+}
